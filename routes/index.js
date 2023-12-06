bb16ec0fad9b9e1fcecaaf98406491c46ee820d9
itsreisv <123587174+itsreisv@users.noreply.github.com>
2023-12-06 13:08:50 -0500

    fcked

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { });
 });
 
 module.exports = router;
